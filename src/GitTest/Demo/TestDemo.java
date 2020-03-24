4bce444361fae2fe0b0b0282dc519fed9a681535
demo20170308 <chinayaoyong@gmail.com>
2020-03-24 20:13:24 +0100

    First Commit

---
+++ src/GitTest/Demo/TestDemo.java
@@ -0,0 +1,10 @@
+package GitTest.Demo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Git demo test...");
+	}
+
+}
